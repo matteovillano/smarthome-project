1b11fe9d7b15a95c95ba5a7d41c0144e15ad0373
matteovillano <villano.1840807@studenti.uniroma1.it>
2020-09-28 13:25:36 +0200

    project started:initial commit

---
+++ smarthome.c
@@ -0,0 +1,16 @@
+#include <util/delay.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+
+//MAIN
+int main(void){
+	
+	while(1){
+		
+	}
+	
+	return 0;
+}
